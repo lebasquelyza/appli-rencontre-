5b11a81af17ec443537b5f66275053448a7296e9
lebasquelyza <lebasquelyza66@gmail.com>
2026-01-26 20:12:46 +0100

    Update capacitor.config.ts

--- sportmeet-complet/capacitor.config.ts
+++ sportmeet-complet/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.sportmeet.app",
-  appName: "SportMeet",
+  appId: "com.matchfit.app",
+  appName: "MatchFit",
   webDir: "dist",
   bundledWebRuntime: false,
   server: {
